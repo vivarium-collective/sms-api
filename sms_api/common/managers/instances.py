d34b2f6257abbc4767860335b0ae487a4380d14d
jcschaff <schaff.jim@gmail.com>
2025-06-06 14:08:56 -0400

    bring in common package from sms-api-gateway

---
+++ sms_api/common/managers/instances.py
@@ -0,0 +1,6 @@
+from common.managers.db import MongoManager, SqlManager
+from common.managers.stream import SocketConnectionManager
+
+mongo_manager = MongoManager()  # TODO: get dynamic url for this
+sql_manager = SqlManager()
+socket_manager = SocketConnectionManager()
