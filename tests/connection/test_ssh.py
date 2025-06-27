9d7842ccb2c6b082fbf1eeced2070f138680787c
Alex Patrie <alexanderpatrie@gmail.com>
2025-06-27 15:48:06 -0400

    updated asyncssh connection test

--- tests/connection/test_ssh.py
+++ tests/connection/test_ssh.py
@@ -1,12 +1,15 @@
+from pathlib import Path
+import sys
 import asyncio
 import os
 import asyncssh
+
 import pytest
 
 
 default_home = '/Users/alexanderpatrie'
 test_key_path = os.path.join(
-    os.getenv('HOME', default_home),
+    os.getenv('HOME', '/Users/alexanderpatrie'),
     '.ssh',
     'sms_id_rsa'
 )
@@ -14,7 +17,7 @@ hpc_host="login.hpc.cam.uchc.edu"
 hpc_username="svc_vivarium"
 
 # @pytest.mark.asyncio
-async def test_asyncssh_connection(host: str, username: str, key_path: str):
+async def test_asyncssh_connection(host: str, username: str, key_path: Path):
     try:
         async with asyncssh.connect(
             host=host,
@@ -22,8 +25,8 @@ async def test_asyncssh_connection(host: str, username: str, key_path: str):
             client_keys=[key_path],
             known_hosts=None  # optional: disables host key checking
         ) as conn:
-            result = await conn.run('echo ping && ls -al', check=True)
-            print(f"Connection successful. Output: {result.stdout.strip()}")
+            result = await conn.run('echo ping && whoami && echo $HOME', check=True)
+            print(f"Connection successful.\nOutput: {result.stdout.strip()}")
             return True
     except (asyncssh.Error, OSError) as e:
         print(f"SSH connection failed: {e}")
@@ -31,10 +34,14 @@ async def test_asyncssh_connection(host: str, username: str, key_path: str):
 
 
 if __name__ == "__main__":
+    args = sys.argv
+    key_path = Path(args[1]) if len(args) > 1 \
+        else Path(os.getenv('HOME', '/Users/alexanderpatrie')) / '.ssh' / 'sms_id_rsa'
+    
     asyncio.run(
         test_asyncssh_connection(
             host="login.hpc.cam.uchc.edu", 
             username="svc_vivarium", 
-            key_path=test_key_path
+            key_path=key_path
         )
     )
