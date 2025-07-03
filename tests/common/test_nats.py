5c5aa122f79e6d398cc3746d20df13a4c11aac06
jcschaff <schaff.jim@gmail.com>
2025-07-02 20:34:51 -0400

    synchronous producer test_nats.py

--- tests/common/test_nats.py
+++ tests/common/test_nats.py
@@ -1,69 +1,9 @@
 import asyncio
+import threading
 
 import pytest
 from nats.aio.client import Client as NATSClient
 from nats.aio.msg import Msg
-
-# from nats.js import JetStreamContext
-
-
-# @pytest.mark.asyncio
-# async def test_jetstream_pubsub(jetstream_client: JetStreamContext) -> None:
-#     # Get a context to produce and consume messages from NATS JetStream
-#     js = jetstream_client
-#
-#     # Create a stream named 'events' and with subjects matching 'events.*'
-#     # 'events' will be a default stream that all events will be sent to
-#     # Storage parameter can be set to 'NONE' for no storage, 'FILE' for file based storage,
-#                                                         or 'MEMORY' for memory based storage
-#     await js.add_stream(name="events", subjects=["events.*"], storage="memory")
-#
-#     # Publish 6 messages to the JetStream
-#     await js.publish("events.page_loaded", b"")
-#     await js.publish("events.mouse_clicked", b"")
-#     await js.publish("events.mouse_clicked", b"")
-#     await js.publish("events.page_loaded", b"")
-#     await js.publish("events.mouse_clicked", b"")
-#     await js.publish("events.input_focused", b"")
-#     print("published 6 messages", "\n")
-#
-#     # Check the number of messages in the stream using streams_info
-#     # StreamState includes the total number of messages in the stream
-#     print(await js.streams_info(), "\n")
-#
-#     # Update the 'events' stream to have a maximum of 10 messages
-#     await js.update_stream(name="events", subjects=["events.*"], max_msgs=10)
-#     print("set max messages to 10", "\n")
-#
-#     # Check the number of messages in the stream using streams_info
-#     # StreamState includes the total number of messages in the stream
-#     print(await js.streams_info(), "\n")
-#
-#     # Update the 'events' stream to have a maximum of 300 bytes
-#     await js.update_stream(name="events", subjects=["events.*"], max_msgs=10, max_bytes=300)
-#     print("set max bytes to 300", "\n")
-#
-#     # Check the number of messages in the stream using streams_info
-#     # StreamState includes the total number of messages in the stream
-#     print(await js.streams_info(), "\n")
-#
-#     # Update the 'events' stream to have a maximum age of 0.1 seconds
-#     await js.update_stream(name="events", subjects=["events.*"], max_msgs=10, max_bytes=300, max_age=0.1)
-#     print("set max age to 0.1 second", "\n")
-#
-#     # Check the number of messages in the stream using streams_info
-#     # StreamState includes the total number of messages in the stream
-#     print(await js.streams_info(), "\n")
-#
-#     # Sleep for 10 seconds to allow messages to expire
-#     await asyncio.sleep(10)
-#
-#     # Check the number of messages in the stream using streams_info
-#     # StreamState includes the total number of messages in the stream
-#     print(await js.streams_info())
-#
-#     # Delete the 'events' stream
-#     await js.delete_stream("events")
 
 
 @pytest.mark.asyncio
@@ -85,3 +25,33 @@ async def test_nats_pubsub(nats_subscriber_client: NATSClient, nats_producer_cli
 
     await asyncio.wait_for(received.wait(), timeout=0.1)
     assert data_holder["data"] == b"hello world"
+
+
+def sync_producer(nats_producer_client: NATSClient) -> None:
+    # This function uses asyncio.run() internally to send a NATS message
+    # Example:
+    for i in range(10):
+        asyncio.run(nats_producer_client.publish("test.subject", b"hello world " + str(i).encode("utf-8")))
+
+
+@pytest.mark.asyncio
+async def test_sync_producer_with_async_subscriber(
+    nats_subscriber_client: NATSClient, nats_producer_client: NATSClient
+) -> None:
+    received = asyncio.Event()
+    data_holder = {}
+
+    async def message_handler(msg: Msg) -> None:
+        data_holder["data"] = msg.data
+        received.set()
+
+    await nats_subscriber_client.subscribe("test.subject", cb=message_handler)
+    await asyncio.sleep(1.0)  # Allow subscription setup
+
+    # Run the synchronous producer in a separate thread
+    thread = threading.Thread(target=sync_producer, args=(nats_producer_client,))
+    thread.start()
+    thread.join()
+
+    await asyncio.wait_for(received.wait(), timeout=1.0)
+    assert data_holder["data"] == b"hello world 9"
