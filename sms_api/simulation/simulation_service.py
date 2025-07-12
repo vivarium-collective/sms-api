89538da2ec5d06916263eef24effa066c6158a64
jcschaff <schaff.jim@gmail.com>
2025-07-12 10:02:20 -0400

    disable bash messaging, rely on emitter

--- sms_api/simulation/simulation_service.py
+++ sms_api/simulation/simulation_service.py
@@ -363,22 +363,22 @@ class SimulationServiceHpc(SimulationService):
                     image="{apptainer_image_path!s}"
                     cd {remote_vEcoli_repo_path!s}
 
-                    # scrape the slurm log file for the magic word and publish to NATS
-                    export NATS_URL={settings.nats_emitter_url}
-                    if [ -z "$NATS_URL" ]; then
-                        echo "NATS_URL environment variable is not set."
-                    else
-                        tail -F {slurm_log_file!s} | while read -r line; do
-                            if echo "$line" | grep -q "{settings.nats_emitter_magic_word}"; then
-                                clean_line="$(echo "$line" | sed \
-                                   -e 's/{settings.nats_emitter_magic_word}//g' \
-                                   -e "s/'/\\&quot;/g" )"
-                                nats pub {settings.nats_worker_event_subject} "'$clean_line'"
-                            fi
-                        done &
-                        SCRAPE_PID=$!
-                        TAIL_PID=$(pgrep -P $SCRAPE_PID tail)
-                    fi
+                    # # scrape the slurm log file for the magic word and publish to NATS
+                    # export NATS_URL={settings.nats_emitter_url}
+                    # if [ -z "$NATS_URL" ]; then
+                    #     echo "NATS_URL environment variable is not set."
+                    # else
+                    #     tail -F {slurm_log_file!s} | while read -r line; do
+                    #         if echo "$line" | grep -q "{settings.nats_emitter_magic_word}"; then
+                    #             clean_line="$(echo "$line" | sed \
+                    #                -e 's/{settings.nats_emitter_magic_word}//g' \
+                    #                -e "s/'/\\&quot;/g" )"
+                    #             nats pub {settings.nats_worker_event_subject} "'$clean_line'"
+                    #         fi
+                    #     done &
+                    #     SCRAPE_PID=$!
+                    #     TAIL_PID=$(pgrep -P $SCRAPE_PID tail)
+                    # fi
 
                     git -C ./configs diff HEAD >> ./source-info/git_diff.txt
                     singularity run $binds $image uv run \\
@@ -389,14 +389,14 @@ class SimulationServiceHpc(SimulationService):
                          --sim_data_path "/parca/{parca_dataset_dirname}/kb/simData.cPickle" \\
                          --fail_at_max_duration
 
-                    if [ -n "$SCRAPE_PID" ]; then
-                        echo "Waiting for scrape to finish..."
-                        sleep 10  # give time for the scrape to finish
-                        kill $SCRAPE_PID || true  # kill the scrape process if it is still running
-                        kill $TAIL_PID || true  # kill the tail process if it is still running
-                    else
-                        echo "No scrape process found."
-                    fi
+                    # if [ -n "$SCRAPE_PID" ]; then
+                    #     echo "Waiting for scrape to finish..."
+                    #     sleep 10  # give time for the scrape to finish
+                    #     kill $SCRAPE_PID || true  # kill the scrape process if it is still running
+                    #     kill $TAIL_PID || true  # kill the tail process if it is still running
+                    # else
+                    #     echo "No scrape process found."
+                    # fi
 
                     # if the experiment directory is empty after the run, fail the job
                     if [ ! "$(ls -A {experiment_path!s})" ]; then
