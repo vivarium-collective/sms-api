399df873d0d7de3ad5fc357b686c6e17de07bfba
jcschaff <schaff.jim@gmail.com>
2025-07-12 14:19:13 -0400

    fix clone test, api does not properly protect against bad commit hashes

--- tests/simulation/test_clone.py
+++ tests/simulation/test_clone.py
@@ -10,11 +10,6 @@ repo_url = "https://github.com/vivarium-collective/vEcoli"
 @pytest.mark.skipif(len(get_settings().slurm_submit_key_path) == 0, reason="slurm ssh key file not supplied")
 @pytest.mark.asyncio
 async def test_clone(simulation_service_slurm: SimulationService, latest_commit_hash: str) -> None:
-    with pytest.raises(ValueError, RuntimeError):
-        await simulation_service_slurm.clone_repository_if_needed(
-            git_commit_hash="invalid_commit_hash", git_repo_url=repo_url, git_branch=main_branch
-        )
-
     await simulation_service_slurm.clone_repository_if_needed(
         git_commit_hash=latest_commit_hash, git_repo_url=repo_url, git_branch=main_branch
     )
