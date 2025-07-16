6c42aab988de7ec79a78c74182529512b48a49eb
Alex Patrie <alexanderpatrie@gmail.com>
2025-07-15 21:31:40 -0400

    savepoint update for plots ui which will replace and become single_cell

--- app/ui/plots.py
+++ app/ui/plots.py
@@ -23,13 +23,16 @@ def _():
     import json
     from pprint import pp
     from pathlib import Path
+    from contextlib import contextmanager
+    from enum import StrEnum
+    from typing import Generator
 
     import marimo as mo
     import polars as pl
     import altair as alt
     from altair import Chart
     from fastapi import HTTPException
-    from httpx import AsyncClient, QueryParams, Client
+    from httpx import AsyncClient, QueryParams, Client, HTTPStatusError, Timeout
 
     from sms_api.simulation.models import (
         SimulatorVersion,
@@ -38,21 +41,76 @@ def _():
         WorkerEvent,
         ParcaDataset
     )
+    from sms_api.config import get_settings
     from app.api.simulations import EcoliSim
     from app.api.client_wrapper import ClientWrapper
+    return (
+        Client,
+        EcoliExperiment,
+        EcoliSimulationRequest,
+        Generator,
+        HTTPStatusError,
+        ParcaDataset,
+        StrEnum,
+        Timeout,
+        WorkerEvent,
+        alt,
+        contextmanager,
+        get_settings,
+        mo,
+        pl,
+    )
 
 
-    base_url = "http://localhost:8888/core"
-    client = ClientWrapper(base_url=base_url)
-    return Client, WorkerEvent, alt, mo, pl
+@app.cell
+def _(Client, Generator, StrEnum, Timeout, contextmanager, get_settings):
+    # -- api client and call setup -- #
+
+    SIMULATION_TEST_ID = 3
+
+    def get_base_url() -> str:
+        settings = get_settings()
+        api_server_url = settings.marimo_api_server
+        if not len(api_server_url):
+            api_server_url = "http://localhost:8000"
+        return f"{api_server_url}/core"
+
+
+    @contextmanager
+    def api_client(base_url: str | None = None) -> Generator[Client, None, None]:
+        """
+        example usage:
+        python```
+            with api_client() as client:
+                url = format_endpoint_url(ApiResource.SIMULATOR, 'versions')
+                resp = client.get(url=url)
+                print(resp.json())
+        ```
+        """
+        with Client(
+            base_url=base_url or get_base_url(),
+            timeout=Timeout(11.11)
+        ) as client:
+            yield client
+
+
+    class ApiResource(StrEnum):
+        SIMULATOR = "simulator"
+        SIMULATION = "simulation"
+
+
+    def format_endpoint_url(resource: ApiResource, *subpaths):
+        base_url = get_base_url()
+        return f"{base_url}/{resource}/{'/'.join(list(subpaths))}"
+
+    # client = ClientWrapper(base_url=base_url)
+    return ApiResource, SIMULATION_TEST_ID, api_client, format_endpoint_url
 
 
 @app.cell
 def _(WorkerEvent, alt, mo, pl):
-    # 1. upload simulator
-    # 2. upload parca
-    # 3. pin {simulator_id: ..., parca_id: ...}
-    # 4. run simulation using #3
+    # -- marimo-specific plot setup and mass listener data selection -- #
+
     COLORS = [
         "#1f77b4",  # blue
         "#ff7f0e",  # orange
@@ -81,7 +139,6 @@ def _(WorkerEvent, alt, mo, pl):
         data: dict[str, int | float | list[int] | dict[str, float]], keys: list[str]
     ) -> dict[str, int | float | list[int] | dict[str, float]]:
         return {key: data.get(key) for key in keys if key in data}
-
 
     def get_events_dataframe(events: list[WorkerEvent] | None = None) -> pl.DataFrame:
         dataframes = []
@@ -143,30 +200,90 @@ def _(WorkerEvent, alt, mo, pl):
 
 
 @app.cell
-def _():
-    # chunks_dir = Path("assets/tests/test_history")
-    # mass_data = get_parquet_mass_data(chunks_dir, MASS_COLUMNS)
-    # simulation_df = get_masses_dataframe(mass_data)
+def _(
+    ApiResource,
+    HTTPStatusError,
+    ParcaDataset,
+    WorkerEvent,
+    api_client,
+    format_endpoint_url,
+    mo,
+):
+    # -- client calls to the API, returning the appropriate DTOs (all with the 'on_' prefix) -- #
+
+    @mo.cache
+    def on_get_parcas() -> list[ParcaDataset]:
+        try:
+            with api_client() as client:
+                url = format_endpoint_url(ApiResource.SIMULATION, 'parca', 'versions')
+                resp = client.get(url=url)
+                resp.raise_for_status()
+                return [ParcaDataset(**dataset) for dataset in resp.json()]
+        except HTTPStatusError as e:
+            raise HTTPStatusError(message=str(e))
+
+    def on_get_worker_events(simulation_id: int) -> list[WorkerEvent]:
+        try:
+            with api_client() as client:
+                url = format_endpoint_url(ApiResource.SIMULATION, 'run', 'events')
+                resp = client.get(url=url, params={"simulation_id": simulation_id})
+                resp.raise_for_status()
+                return [WorkerEvent(**event) for event in resp.json()]
+        except HTTPStatusError as e:
+            raise HTTPStatusError(message=str(e))
+
+
+    try:
+        parca_datasets = on_get_parcas()
+    except ValueError as e:
+        print(e)
+        parca_datasets = None
+    return on_get_worker_events, parca_datasets
+
+
+@app.cell
+def _(
+    EcoliExperiment,
+    EcoliSimulationRequest,
+    ParcaDataset,
+    SIMULATION_TEST_ID,
+):
+    # TODO: simulation id comes from simulation run DTO (EcoliExperiment)
+    # placeholder: simulation_id = experiment.database_id
+
+    def get_simulation_id(experiment: EcoliExperiment | None = None) -> int:
+        if experiment:
+            return experiment.simulation.database_id
+        else:
+            return SIMULATION_TEST_ID
+
+
+    def extract_simulation_request(
+        parca_datasets: list[ParcaDataset],
+        variant_config: dict[str, dict[str, float]] | None = None  # TODO: formalize this
+    ) -> EcoliSimulationRequest:
+        if not len(parca_datasets):
+            raise ValueError('There are no datasets uploaded')
+        active_parca_dataset: ParcaDataset = parca_datasets[-1]
+        return EcoliSimulationRequest(
+            parca_dataset_id=active_parca_dataset.database_id,
+            simulator=active_parca_dataset.parca_dataset_request.simulator_version,
+            variant_config=variant_config or {"named_parameters": {"param1": 0.5, "param2": 0.5}}
+        )
+    return extract_simulation_request, get_simulation_id
+
+
+@app.cell
+def _(extract_simulation_request, parca_datasets):
+    request = extract_simulation_request(parca_datasets)
+    request
     return
 
 
 @app.cell
-def _(Client):
-    def get_worker_events(simulation_id: int):
-        with Client() as _client:
-            resp = _client.get(url="http://localhost:8888/core/simulation/run/events", params={"simulation_id": 3})
-            resp.raise_for_status()
-            return resp.json()
-
-    # placeholder: simulation_id = experiment.database_id
-    simulation_id = 3
-    worker_event_data = get_worker_events(simulation_id)
-    return (worker_event_data,)
-
-
-@app.cell
-def _(WorkerEvent, get_events_dataframe, worker_event_data):
-    worker_events = [WorkerEvent(**event) for event in worker_event_data]
+def _(get_events_dataframe, get_simulation_id, on_get_worker_events):
+    simulation_id = get_simulation_id()
+    worker_events = on_get_worker_events(simulation_id)
     simulation_df = get_events_dataframe(worker_events)
     return (simulation_df,)
 
