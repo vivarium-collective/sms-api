24cc604fabd2dcebd6270b9e16958f5171e76010
Alex Patrie <alexanderpatrie@gmail.com>
2025-08-22 11:03:15 -0400

    fix: remove unused test

--- tests/data/test_biocyc_service.py
+++ tests/data/test_biocyc_service.py
@@ -1,7 +1,7 @@
 import pytest
 import requests
 
-from sms_api.data.biocyc_service import get_biocyc_data, load_flat
+from sms_api.data.biocyc_service import get_biocyc_data
 
 
 @pytest.mark.asyncio
@@ -11,10 +11,3 @@ async def test_get_biocyc_data(session: requests.Session) -> None:
     data = get_biocyc_data(session, orgid, objid)
     assert data is not None
     print(data)
-
-
-@pytest.mark.asyncio
-async def test_load_flat() -> None:
-    metabolits = load_flat("metabolites")
-    metabolite_ids = metabolits[["id"]].to_numpy().flatten()
-    assert len(metabolite_ids)
