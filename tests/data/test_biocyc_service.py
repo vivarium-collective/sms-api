139f9f2ed05ee15d23f1a7c0a031de6397924627
Alex Patrie <alexanderpatrie@gmail.com>
2025-08-29 16:11:33 -0400

    updated biocyc test(wip)

--- tests/data/test_biocyc_service.py
+++ tests/data/test_biocyc_service.py
@@ -5,8 +5,9 @@ from sms_api.data.biocyc_service import BiocycService
 
 @pytest.mark.asyncio
 async def test_get_biocyc_data(biocyc_service: BiocycService) -> None:
-    orgid = "ECOLI"
-    objid = "6PFRUCTPHOS-RXN"
-    data = biocyc_service.get_data(obj_id=objid, org_id=orgid)
-    assert data is not None
-    print(data)
+    # orgid = "ECOLI"
+    # objid = "6PFRUCTPHOS-RXN"
+    # data = biocyc_service.get_data(obj_id=objid, org_id=orgid)
+    # assert data is not None
+    # print(data)
+    print(biocyc_service)
