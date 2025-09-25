a076382c3d019978fb64fb8749525f698ef0c9c4
Alex Patrie <alexanderpatrie@gmail.com>
2025-09-25 10:07:38 -0400

    updated existing routers and removed wcm router

--- sms_api/api/routers/antibiotics.py
+++ sms_api/api/routers/antibiotics.py
@@ -4,4 +4,4 @@ from sms_api.common.gateway.utils import router_config
 
 logger = logging.getLogger(__name__)
 
-config = router_config(prefix="variants")
+config = router_config(prefix="antibiotics")
