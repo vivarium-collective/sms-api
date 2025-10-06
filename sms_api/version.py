2e7a88c7213d19b71f4c5a0f3c38eeeef61e78f4
Alex Patrie <alexanderpatrie@gmail.com>
2025-10-06 11:52:03 -0400

    chore: bump version & increase ssh proxy timeouts

--- sms_api/version.py
+++ sms_api/version.py
@@ -1,5 +1,5 @@
 # NEWEST(FIRST NEXTFLOW): "0.2.11-dev"
 # STABLE: "0.2.10-dev"
 # PREVIOUS STABLE: "0.2.8"
-# LATEST STABLE: "0.2.25-dev"
-__version__ = "0.2.26-dev"
+# LATEST STABLE: "0.2.26-dev"
+__version__ = "0.2.27-dev"
