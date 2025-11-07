6eee70e761679f23eb831a15bbc1a11939c9be1e
jcschaff <schaff.jim@gmail.com>
2025-11-07 12:20:52 -0500

    add support for alembic migrations in cluster

--- kustomize/scripts/migrate_kubernetes.sh
+++ kustomize/scripts/migrate_kubernetes.sh
@@ -5,3 +5,9 @@ if [ $? -ne 0 ]; then
   echo "Error applying kustomize configuration. Please check your setup."
   exit 1
 fi
+
+kubectl wait --for=condition=complete job/alembic-migrate
+if [ $? -ne 0 ]; then
+  echo "Error waiting for alembic-migrate job to complete. Please check the job status."
+  exit 1
+fi
