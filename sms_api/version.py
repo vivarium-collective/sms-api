1eeff42c2b7a21cc60a11b3a3ac77d3ccfec79ad
Alex Patrie <alexanderpatrie@gmail.com>
2025-12-11 17:52:37 -0500

    bump to v0.2.59-dev for splash/app changes

--- sms_api/version.py
+++ sms_api/version.py
@@ -1,5 +1,5 @@
 # NEWEST(FIRST NEXTFLOW): "0.2.11-dev"
 # STABLE: "0.2.10-dev"
 # PREVIOUS STABLE: "0.2.8"
-# LATEST STABLE: "0.2.57-dev"
-__version__ = "0.2.58-dev"
+# LATEST STABLE: "0.2.58-dev" (original preferred analyses) OR "0.2.57-dev" (ultra-cached analyses)
+__version__ = "0.2.59-dev"
