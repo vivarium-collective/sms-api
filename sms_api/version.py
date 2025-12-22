fa10c45c347b4663a89486d3d55a32f5a2a62b83
jcschaff <schaff.jim@gmail.com>
2025-12-22 15:41:15 -0500

    bump version to 0.2.56.dev1 and regenerate secrets including for FSx

--- sms_api/version.py
+++ sms_api/version.py
@@ -1,5 +1,5 @@
 # NEWEST(FIRST NEXTFLOW): "0.2.11-dev"
 # STABLE: "0.2.10-dev"
 # PREVIOUS STABLE: "0.2.8"
-# LATEST STABLE: "0.2.57-dev"
-__version__ = "0.2.74-dev0"
+# LATEST STABLE: "0.2.56.dev1"
+__version__ = "0.2.56.dev1"
