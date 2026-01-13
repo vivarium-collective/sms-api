281b6bdadf4c6dd941dca4a1f1b7e55e37c8bb54
Alex Patrie <alexanderpatrie@gmail.com>
2026-01-13 15:43:26 -0500

    used analysis outdir test fixture and removed unused nextflow content

--- tests/common/handlers/test_simulations_handler.py
+++ tests/common/handlers/test_simulations_handler.py
@@ -1,5 +1,3 @@
-from pathlib import Path
-
 import pytest
 
 from sms_api.common.handlers.simulations import fetch_omics_outputs, get_available_omics_output_paths
@@ -10,8 +8,9 @@ from sms_api.config import get_settings
 
 @pytest.mark.skipif(len(get_settings().slurm_submit_key_path) == 0, reason="slurm ssh key file not supplied")
 @pytest.mark.asyncio
-async def test_get_available_omics_output_paths(ssh_session_service: SSHSessionService) -> None:
-    analysis_outdir = HPCFilePath(remote_path=Path("/projects/SMS/sms_api/alex/sims/sms_multigeneration/analyses"))
+async def test_get_available_omics_output_paths(
+    ssh_session_service: SSHSessionService, analysis_outdir: HPCFilePath
+) -> None:
     results = await get_available_omics_output_paths(remote_analysis_outdir=analysis_outdir)
     assert len(results), "No files found."
     assert all([isinstance(fp, HPCFilePath) and fp.remote_path.__str__().endswith(".txt") for fp in results])
@@ -19,7 +18,8 @@ async def test_get_available_omics_output_paths(ssh_session_service: SSHSessionS
 
 @pytest.mark.skipif(len(get_settings().slurm_submit_key_path) == 0, reason="slurm ssh key file not supplied")
 @pytest.mark.asyncio
-async def test_fetch_simulation_omics_outputs(ssh_session_service: SSHSessionService) -> None:
-    analysis_outdir = HPCFilePath(remote_path=Path("/projects/SMS/sms_api/alex/sims/sms_multigeneration/analyses"))
+async def test_fetch_simulation_omics_outputs(
+    ssh_session_service: SSHSessionService, analysis_outdir: HPCFilePath
+) -> None:
     results = await fetch_omics_outputs(exp_analysis_outdir=analysis_outdir)
     assert len(results)
