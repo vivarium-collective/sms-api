d4d8f117956af32b8ccf15e64103b230c53545e4
Alex Patrie <alexanderpatrie@gmail.com>
2026-02-01 17:33:54 -0500

    bump to version 0.4.7 for repo and 0.4.7-dev for rke-dev api img

--- sms_api/version.py
+++ sms_api/version.py
@@ -1,5 +1,6 @@
 # NEWEST(FIRST NEXTFLOW): "0.2.11-dev"
 # STABLE: "0.2.10-dev"
 # PREVIOUS STABLE: "0.2.8"
-# LATEST STABLE: "0.2.74-dev"
-__version__ = "0.4.6"
+# LATEST STABLE (old): "0.2.74-dev"
+# LATEST STABLE (most recent before hackathon 1): "0.4.6"
+__version__ = "0.4.7"
