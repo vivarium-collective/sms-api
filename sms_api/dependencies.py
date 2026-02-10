9aa5efa4d09b5cf41138b277a5b1e82f174ced85
Alex Patrie <alexanderpatrie@gmail.com>
2026-02-10 13:49:37 -0800

    fix: remove dir cleanup

--- sms_api/dependencies.py
+++ sms_api/dependencies.py
@@ -1,5 +1,4 @@
 import logging
-import shutil
 from pathlib import Path
 from typing import TYPE_CHECKING, Any
 
@@ -13,7 +12,7 @@ from sms_api.common.storage.file_service import FileService
 from sms_api.common.storage.file_service_gcs import FileServiceGCS
 from sms_api.common.storage.file_service_qumulo_s3 import FileServiceQumuloS3
 from sms_api.common.storage.file_service_s3 import FileServiceS3
-from sms_api.config import REPO_ROOT, get_settings
+from sms_api.config import get_settings
 from sms_api.log_config import setup_logging
 from sms_api.simulation.database_service import DatabaseService, DatabaseServiceSQL
 from sms_api.simulation.tables_orm import create_db
@@ -288,5 +287,5 @@ async def shutdown_standalone() -> None:
     if job_scheduler:
         await job_scheduler.close()
         set_job_scheduler(None)
-    for dirpath in [p for p in Path(f"{REPO_ROOT}/.results_cache").rglob("*") if p.is_dir()]:
-        shutil.rmtree(dirpath)
+    # for dirpath in [p for p in Path(f"{REPO_ROOT}/.results_cache").rglob("*") if p.is_dir()]:
+    #     shutil.rmtree(dirpath)
