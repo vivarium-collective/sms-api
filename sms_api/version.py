bc5a95b25473c5556deed83ec622881e461102c2
Alex Patrie <alexanderpatrie@gmail.com>
2026-04-09 20:43:21 -0400

    chore: bump version and update kustomize image tags

--- sms_api/version.py
+++ sms_api/version.py
@@ -3,4 +3,5 @@
 # PREVIOUS STABLE: "0.2.8"
 # LATEST STABLE (old): "0.2.74-dev"
 # LATEST STABLE (most recent before hackathon 1): "0.4.8" -> 0.5.9
-__version__ = "0.6.0"
+# LATEST STABLE AFTER HACKATHON FOR AWS: 0.6.0
+__version__ = "0.6.1"
